7c2f7b12e18a98eb595d527a3485f22e0659c8ec
Shreejan Maharjan <105190748+Ashura2003@users.noreply.github.com>
2022-12-28 10:49:15 +0545

    Adjusted

--- src/controller/HnSController.java
+++ src/controller/HnSController.java
@@ -7,13 +7,14 @@ public class HnSController {
     DbConnection dbConnection;
 
     public int insertDetails(helpnsupportm hns) {
-        String name = hns.getname();
+        String name = hns.getName();
         String contact = hns.getContact();
         String address = hns.getAddress();
         String email = hns.getEmail();
+        String feedback=hns.getFeedback();
 
-        String insert = "insert into helpnsupport(name,contact,address,email)" + "values('" + name + "','" + contact
-                + "','" + address + "','" + email + "')";
+        String insert = "insert into helpnsupport(name,contact,address,email,feedback)" + "values('" + name + "','" + contact
+                + "','" + address + "','" + email +"','" + feedback + "')";
         dbConnection = new DbConnection();
         int Result = dbConnection.manipulate(insert);
         return Result;
