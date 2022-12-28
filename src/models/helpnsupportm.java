7c4819f2e62b42205e605e8a1a17276d7a16c115
Shreejan Maharjan <105190748+Ashura2003@users.noreply.github.com>
2022-12-28 10:50:32 +0545

    Complete

--- src/models/helpnsupportm.java
+++ src/models/helpnsupportm.java
@@ -1,56 +1,61 @@
 package models;
 
+
+
 public class helpnsupportm {
-    private String name;
-    private String contact;
-    private String address;
-    private String email;
+    private static final String Contact = null;
+    private String Name;
+    private String SContact;
+    private String Address;
+    private String Email;
+    private String Feedback;
 
-    public helpnsupportm() {
-        String name;
-        String contact;
-        String address;
-        String email;
+    public helpnsupportm(String name2, String sContact2, String address2, String email2, String feedback2){
+        
+        this.Name=name2;
+        this.SContact=sContact2;
+        this.Address=address2;
+        this.Email=email2;
+        this.Feedback=feedback2;
     }
 
-    {
-        this.name = name;
-        this.contact = contact;
-        this.address = address;
-        this.email = email;
-
+    public void setName(String newName){
+        this.Name=newName;
     }
 
-    public void setName(String newName) {
-        this.name = newName;
+    public String getName(){
+        return this.Name;
     }
 
-    public String getname() {
-        return this.name;
+    public void setContact(String newContact){
+        this.SContact=newContact;
     }
 
-    public void setContact(String newContact) {
-        this.address = newContact;
+    public String getContact(){
+        return this.Contact;
+    }
+    
+    public void setAddress(String newAddress){
+        this.Address=newAddress;
     }
 
-    public String getContact() {
-        return this.contact;
+    public String getAddress(){
+        return this.Address;
+    }
+    
+    public void setEmail(String newEmail){
+        this.Email=newEmail;
     }
 
-    public void setAddress(String newAddress) {
-        this.contact = newAddress;
+    public String getEmail(){
+        return this.Email;
+    }
+    
+    public void setFeedback(String newFeedback){
+        this.Feedback=newFeedback;
     }
 
-    public String getAddress() {
-        return this.contact;
+    public String getFeedback(){
+        return this.Feedback;
     }
-
-    public void setEmail(String newEmail) {
-        this.email = newEmail;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
 }
