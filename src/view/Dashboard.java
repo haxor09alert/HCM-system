cb2b342d81a21e68e556eddcf901b571887dd13f
Shreejan Maharjan <105190748+Ashura2003@users.noreply.github.com>
2022-12-28 09:30:32 +0545

    complete

--- src/view/Dashboard.java
+++ src/view/Dashboard.java
@@ -1,7 +1,10 @@
+package view;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+
 
 
 /**
@@ -26,32 +29,133 @@ public class Dashboard extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        MenuPanel = new javax.swing.JPanel();
+        UpdatesButton = new javax.swing.JButton();
+        AboutUs = new javax.swing.JButton();
+        Appointments = new javax.swing.JButton();
+        Login = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(1920, 1080));
+        getContentPane().setLayout(null);
 
-        jLabel1.setFont(new java.awt.Font("Verdana", 1, 18)); // NOI18N
-        jLabel1.setText("Dashboard Proxy");
+        MenuPanel.setBackground(new java.awt.Color(123, 100, 148));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(139, 139, 139)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(158, Short.MAX_VALUE))
+        UpdatesButton.setBackground(new java.awt.Color(123, 100, 148));
+        UpdatesButton.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 24)); // NOI18N
+        UpdatesButton.setText("UPDATES");
+        UpdatesButton.setBorder(null);
+        UpdatesButton.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        UpdatesButton.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        UpdatesButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                UpdatesButtonActionPerformed(evt);
+            }
+        });
+
+        AboutUs.setBackground(new java.awt.Color(123, 100, 148));
+        AboutUs.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 24)); // NOI18N
+        AboutUs.setText("ABOUT US");
+        AboutUs.setActionCommand("");
+        AboutUs.setBorder(null);
+        AboutUs.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        AboutUs.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        AboutUs.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AboutUsActionPerformed(evt);
+            }
+        });
+
+        Appointments.setBackground(new java.awt.Color(123, 100, 148));
+        Appointments.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 24)); // NOI18N
+        Appointments.setText("APPOINTMENTS");
+        Appointments.setToolTipText("");
+        Appointments.setActionCommand("");
+        Appointments.setBorder(null);
+        Appointments.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        Appointments.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        Appointments.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AppointmentsActionPerformed(evt);
+            }
+        });
+
+        Login.setBackground(new java.awt.Color(123, 100, 148));
+        Login.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 24)); // NOI18N
+        Login.setText("LOGIN");
+        Login.setBorder(null);
+        Login.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        Login.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        Login.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                LoginActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout MenuPanelLayout = new javax.swing.GroupLayout(MenuPanel);
+        MenuPanel.setLayout(MenuPanelLayout);
+        MenuPanelLayout.setHorizontalGroup(
+            MenuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(MenuPanelLayout.createSequentialGroup()
+                .addContainerGap(64, Short.MAX_VALUE)
+                .addComponent(UpdatesButton, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(66, 66, 66)
+                .addComponent(AboutUs)
+                .addGap(63, 63, 63)
+                .addComponent(Appointments, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(41, 41, 41)
+                .addComponent(Login, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(47, 47, 47))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(115, 115, 115)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(140, Short.MAX_VALUE))
+        MenuPanelLayout.setVerticalGroup(
+            MenuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, MenuPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(MenuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(Appointments, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(AboutUs, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 48, Short.MAX_VALUE)
+                    .addComponent(UpdatesButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(Login, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
+
+        getContentPane().add(MenuPanel);
+        MenuPanel.setBounds(700, 100, 840, 60);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("/view/logo.jpg")); // NOI18N
+        jLabel1.setText("");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(20, 60, 200, 200);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/BG_frame.png"))); // NOI18N
+        jLabel2.setText("jLabel2");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(0, 0, 1610, 810);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void UpdatesButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_UpdatesButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_UpdatesButtonActionPerformed
+
+    private void AboutUsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AboutUsActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_AboutUsActionPerformed
+
+    private void AppointmentsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AppointmentsActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_AppointmentsActionPerformed
+
+    private void LoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LoginActionPerformed
+        // TODO add your handling code here:
+        
+        dispose();
+        Login log=new Login();
+        log.show();
+    }//GEN-LAST:event_LoginActionPerformed
 
     /**
      * @param args the command line arguments
@@ -89,6 +193,12 @@ public class Dashboard extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton AboutUs;
+    private javax.swing.JButton Appointments;
+    private javax.swing.JButton Login;
+    private javax.swing.JPanel MenuPanel;
+    private javax.swing.JButton UpdatesButton;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
